e4c998f8ad935e56bc224d44323e5971af172f9e
sanjina-stha <sanjina.aa@yahoo.com>
2026-02-09 13:51:30 +0545

    backend project setup

---
+++ controller/userController.js
@@ -0,0 +1,16 @@
+// import user from "../model/userModel"
+
+const User = require("../model/userModel")
+
+const register = async(req, res) => {  //frontend bata backend ma data send garne
+
+}
+
+const login = async (req, res) => {
+
+}
+
+module.exports = {
+  register,
+  login
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('Hello, World!');
+
+//Today we will study about MVC (Model View Controller) clean architecture and publish to github
+
+//At first, we will learn about userside...!
